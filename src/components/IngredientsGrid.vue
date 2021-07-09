2b9ddab06515341e12b5079c6d57455342d0883b
Jeremy Udit <jcudit@github.com>
2021-07-09 13:22:53 -0400

    feat: add ratio adjustment

--- src/components/IngredientsGrid.vue
+++ src/components/IngredientsGrid.vue
@@ -53,7 +53,10 @@
         <v-card class="pa-8" tile>
           <v-row>
             <v-col>
-              <span class="text-h3 font-weight-light" v-text="flour.toFixed(2)"></span>
+              <span
+                class="text-h3 font-weight-light"
+                v-text="flour.toFixed(1)"
+              ></span>
               <span class="subheading font-weight-light">grams</span>
             </v-col>
             <v-col>
@@ -67,7 +70,62 @@
       </v-col>
     </v-row>
 
+    <v-row>
+      <v-col>
+        <div class="text-center">
+          <v-btn color="deep-purple" dark @click="sheet = !sheet">
+            Adjust Ratios
+          </v-btn>
+          <v-bottom-sheet v-model="sheet">
+            <v-sheet class="text-center" height="200px">
+              <div>
+                <v-slider
+                  v-model="activeRatios.salt"
+                  :label="'salt'"
+                  thumb-label="always"
+                  class="px-4 pt-4"
+                  color="deep-purple"
+                ></v-slider>
 
+                <v-slider
+                  v-model="activeRatios.water"
+                  :label="'water'"
+                  thumb-label="always"
+                  class="px-4"
+                  color="deep-purple"
+                ></v-slider>
+
+                <v-slider
+                  v-model="activeRatios.starter"
+                  :label="'starter'"
+                  thumb-label="always"
+                  class="px-4 pb-0"
+                  color="deep-purple"
+                ></v-slider>
+              </div>
+
+              <v-row>
+                <v-col>
+                  <v-btn
+                    class="pb-8"
+                    text
+                    color="deep-purple"
+                    @click="resetRatios"
+                  >
+                    reset
+                  </v-btn>
+                </v-col>
+                <v-col>
+                  <v-btn class="pb-8" text color="red" @click="sheet = !sheet">
+                    close
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-sheet>
+          </v-bottom-sheet>
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -75,17 +133,38 @@
 export default {
   data: () => ({
     flour: 40,
+    sheet: false,
+
+    defaultRatios: {
+      salt: 25,
+      water: 80,
+      starter: 40,
+    },
+
+    activeRatios: {
+      salt: 25,
+      water: 80,
+      starter: 40,
+    },
   }),
 
   computed: {
     salt() {
-      return (0.25 * this.flour).toFixed(2);
+      return ((this.activeRatios.salt / 100) * this.flour).toFixed(1);
     },
     starter() {
-      return (0.8 * this.flour).toFixed(2);
+      return ((this.activeRatios.starter / 100) * this.flour).toFixed(1);
     },
     water() {
-      return (0.4 * this.flour).toFixed(2);
+      return ((this.activeRatios.water / 100) * this.flour).toFixed(1);
+    },
+  },
+
+  methods: {
+    resetRatios() {
+      for (const key in this.defaultRatios) {
+        this.activeRatios[key] = this.defaultRatios[key];
+      }
     },
   },
 };
